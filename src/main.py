59e2053f0dcfb9e98877dadcbb7c230021f0bcda
Kamil <kamilttt271@gmail.com>
2024-09-23 08:57:52 +0200

    Add textnode.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    node1 = TextNode("My Text", "bold", "http://sth.com")
+    print(node1.__repr__())
+
+if __name__ == "__main__":
+    main()
