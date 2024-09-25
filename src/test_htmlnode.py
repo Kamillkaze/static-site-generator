d990423d7c75e393c9876ca2e7112903abdf4fc2
Kamil <kamilttt271@gmail.com>
2024-09-25 08:42:30 +0200

    Add tests for HTMLNode

---
+++ src/test_htmlnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        props = { "href": "https://www.google.com", "target": "_blank", }
+        node = HTMLNode("tag", "value", None, props)
+        expected = " href=\"https://www.google.com\" target=\"_blank\""
+        result = node.props_to_html()
+
+        self.assertEqual(result, expected)
+
+    def test_props_to_html_not_equals(self):
+        props = { "href": "www.google.com", "target": "_blank", }
+        node = HTMLNode("tag", "value", None, props)
+        expected = " href=\"https://www.google.com\" target=\"_blank\""
+        result = node.props_to_html()
+
+        self.assertNotEqual(result, expected)
+
+    def test_props_to_html_props_empty(self):
+        props = {} 
+        node = HTMLNode("tag", "value", None, props)
+        expected = ""
+        result = node.props_to_html()
+
+        self.assertEqual(result, expected)
+    
