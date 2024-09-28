4bc9acedec346aee19d1a0e00e5052d369f9d4e4
Kamil <kamilttt271@gmail.com>
2024-09-28 10:38:15 +0200

    Add to_html implementation for LeafNode

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,3 +3,10 @@ from htmlnode import HTMLNode
 class LeafNode(HTMLNode):
     def __init__(self, value, tag=None, props=None):
         super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("A leaf node must have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
