b7a7051ffbcb3bd6dc2f058b4f48f3d7bc4bb4c6
Kamil <kamilttt271@gmail.com>
2024-09-28 11:11:01 +0200

    Create parentnode class

---
+++ src/parentnode.py
@@ -0,0 +1,8 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.value}, {self.props})"
