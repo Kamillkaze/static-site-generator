5559419eeec3b13f63abdce00deac4e719941ad0
Kamil <kamilttt271@gmail.com>
2024-10-02 18:42:02 +0200

    Add tests for ParentNode

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -6,8 +6,43 @@ from leafnode import LeafNode
 class TestParentNode(unittest.TestCase):
 
     def test_to_html(self):
-        parent_node = ParentNode("p",[LeafNode("b", "Bold text"),LeafNode(None, "Normal text"),LeafNode("i", "italic text"),LeafNode(None, "Normal text"),],)
-        result = parent_node.to_html()
+        parent_node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
         expected = "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>"
+
+        result = parent_node.to_html()
+
         self.assertEqual(result, expected)
     
+    def test_to_html_headings(self):
+        parent_node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        expected = "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>"
+        
+        result = parent_node.to_html()
+
+        self.assertEqual(result, expected)
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        expected = "<div><span><b>grandchild</b></span></div>"
+        
+        result = parent_node.to_html()
+
+        self.assertEqual(result, expected)
