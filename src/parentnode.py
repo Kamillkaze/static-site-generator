d1b43d0e26202a60dca3579da43e041e19d51b86
Kamil <kamilttt271@gmail.com>
2024-10-03 08:34:14 +0200

    Move repr down

--- src/parentnode.py
+++ src/parentnode.py
@@ -3,9 +3,6 @@ from htmlnode import HTMLNode
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
-
-    def __repr__(self):
-        return f"ParentNode({self.tag}, {self.value}, {self.props})"
     
     def to_html(self):
         if self.tag is None:
@@ -16,3 +13,6 @@ class ParentNode(HTMLNode):
         for child in self.children:
             childrenHtml += child.to_html()
         return f"<{self.tag}>{childrenHtml}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.value}, {self.props})"
