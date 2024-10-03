15eb686de1b58403ac1eb3f375deb4e6923a735e
Kamil <kamilttt271@gmail.com>
2024-10-03 09:04:30 +0200

    Add text_node_to_html_node() method

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from leafnode import LeafNode
+
 text_type_text = "text"
 text_type_bold = "bold"
 text_type_italic = "italic"
@@ -10,6 +12,30 @@ class TextNode:
         self.text = text
         self.text_type = text_type
         self.url = url
+
+    def text_node_to_html_node(text_node):
+        leafnode = LeafNode(None, None)
+        if text_node.text_type == text_type_image:
+            leafnode.tag = "img"
+            leafnode.value = ""
+            leafnode.props = { "src": f"{text_node.url}", "alt": f"{text_node.text}" }
+        elif text_node.text_type == text_type_text:
+            leafnode.tag = None
+        elif text_node.text_type == text_type_bold:
+            leafnode.tag = "b"
+        elif text_node.text_type == text_type_italic:
+            leafnode.tag = "i"
+        elif text_node.text_type == text_type_code:
+            leafnode.tag = "code"
+        elif text_node.text_type == text_type_link:
+            leafnode.tag = "a"
+            leafnode.props = { "href": f"{text_node.url}" }
+        else:
+            raise Exception("type not supported") 
+        
+        leafnode.value = text_node.text
+
+        return leafnode
 
     def __eq__(self, other):
         return (
